47e663a914e20afeead3829d0218d3bef3228b36
Vladyslav Nepomniashchyi <vladyslav@Vladyslavs-MacBook-Pro.local>
2025-07-11 01:40:12 +0100

    update messages

--- handlers/commands.py
+++ handlers/commands.py
@@ -8,7 +8,7 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply(
-        "Привет! Отправь мне ссылку из TikTok, YouTube или Instagram — и я скачаю видео для тебя."
+        "Привет! Отправь мне ссылку из TikTok и я скачаю видео для тебя!"
     )
 
 @router.message(lambda message: any(domain in message.text.lower() for domain in ["tiktok.com", "youtube.com", "instagram.com"]))
@@ -16,3 +16,13 @@ async def video_request(message: Message):
     url = message.text.strip()
     await message.answer("Скачиваю видео, подожди...")
     await hf.download_and_send_media(message.bot, message.chat.id, url)
+
+
+@router.message(lambda message: any(domain in message.text.lower() for domain in ["tiktok.com", "youtube.com", "instagram.com"]))
+async def video_request(message: Message):
+    url = message.text.strip()
+    status_msg = await message.answer("Скачиваю видео, подожди...")
+    
+    await hf.download_and_send_media(message.bot, message.chat.id, url)
+    await message.answer("Вот твоё видео 🎬")
+    await message.bot.delete_message(chat_id=message.chat.id, message_id=status_msg.message_id)
