6d5cf0a3ee0ebacb48653e27aef65d65194ed689
Vladyslav Nepomniashchyi <vladyslav@Vladyslavs-MacBook-Pro.local>
2025-07-11 01:24:33 +0100

    update alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def home():
     return "Бот живой 🔥", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
